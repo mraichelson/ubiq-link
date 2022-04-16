87170db1e5a1f19d4a6235904b9c56e93e4bde3f
Michael Raichelson <i.am@michaelraichelson.com>
2022-04-16 18:27:34 -0500

    Add keys to feed embed items to help sort out the animations

--- pages/embed/index.vue
+++ pages/embed/index.vue
@@ -6,29 +6,35 @@
           class="w-[600px] absolute left-[50%] bottom-0 translate-x-[-300px]"
           v-if="item.type === 'crew'"
           :content="item"
+          :key="`crew-${item.slug}`"
         />
         <card-lifeline
           class="w-[600px] absolute left-[50%] bottom-0 translate-x-[-300px]"
           v-else-if="item.type === 'lifeline'"
           :content="item"
+          :key="`lifeline-${item.slug}`"
         />
         <card-post
           class="w-[600px] absolute left-[50%] bottom-0 translate-x-[-300px]"
           v-else-if="item.type === 'post'"
           :content="item"
+          :key="`post-${item.slug}`"
         />
         <card-comment
           class="w-[600px] absolute left-[50%] bottom-0 translate-x-[-300px]"
           v-else-if="item.type === 'comment'"
           :content="item"
+          :key="`comment-${item.slug}`"
         />
         <card-share
           class="w-[600px] absolute left-[50%] bottom-0 translate-x-[-300px]"
           v-else-if="item.type === 'share'"
           :content="item"
+          :key="`share-${item.slug}`"
         />
         <card-ad
           class="w-[600px] absolute left-[50%] bottom-0 translate-x-[-300px]"
+          :key="`ad-${item.slug}`"
           v-else
         />
       </transition>
